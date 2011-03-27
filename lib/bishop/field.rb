9976b5d3e55ca519202aef8c4fb7ec9a449f8b99
brodie hanbuch <brodie.hanbuch@gmail.com>
2011-03-27 12:32:52 -0400

    refactoring

--- lib/bishop/field.rb
+++ lib/bishop/field.rb
@@ -2,12 +2,12 @@ module Bishop
   class Field
     attr_accessor :name, :type, :minOccurs, :nillable, :is_primitive, :java_type, :col_name, :sql_type
 
-    def initialize(field_info)
+    def initialize(field_info={})
       # @minOccurs = 0
-      @name = field_info['name']
-      @type = field_info['type']
+      @name = field_info['name'] || ''
+      @type = field_info['type'] || ''
       @minOccurs = field_info['minOccurs'] || 0
-      @nillable = field_info['nillable']
+      @nillable = field_info['nillable'] || false
     end
 
     def pluralize
