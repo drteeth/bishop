6d4ba259f2c2b2b429d9b152b258ae012a9d98f4
Benjamin Moss <ben@indusblue.com>
2011-04-04 16:17:14 -0400

    added sencha touch model template

--- lib/bishop/java_hammer.rb
+++ lib/bishop/java_hammer.rb
@@ -7,7 +7,7 @@ module Bishop
       @output_folder = options[:output]
       @provider_template = ERB.new(File.read(File.join( File.dirname(__FILE__), '../../templates/content-provider.java.erb')))
       @dto_template = ERB.new(File.read(File.join( File.dirname(__FILE__), '../../templates/dto.java.erb')))
-      #@dao_template = ERB.new(File.read(File.join( File.dirname(__FILE__), '../../templates/dto.java.erb')))
+      @sencha_template = ERB.new(File.read(File.join( File.dirname(__FILE__), '../../templates/model.sencha.js.erb')))
       @test_template = ERB.new(File.read(File.join( File.dirname(__FILE__), '../../templates/test.java.erb')))
       @pattern_map = PatternMap.new
     end    
@@ -47,7 +47,7 @@ module Bishop
         write_file( "#{@namespace}.provider.#{java_class.name}Provider", @provider_template.result(get_binding) )
         write_file( "#{@namespace}.model.#{java_class.name}", @dto_template.result(get_binding) )
         write_file( "#{@namespace}.provider.test.#{java_class.name}ProviderTest", @test_template.result(get_binding) )
-        #write_file( "#{@namespace}.model.dao.#{java_class.name}Dao", @dao_template.result(get_binding) )
+        write_file( "sencha.models.#{java_class.name.downcase}", @sencha_template.result(get_binding) )
         
       end
     end
